681c85c43ae64558e0494343a6ebd03dc3743db9
miwanoff <marina.iva.noffa@gmail.com>
2023-11-19 12:36:48 +0200

    Add Rectangle

--- script.js
+++ script.js
@@ -11,9 +11,19 @@ let coords = document.getElementById("coords");
 console.log(map.width);
 wrap.style.width = `${map.width}px`;
 wrap.style.height = "570px";
+wrap.style.position = "relative";
 
 map.onmousemove = function (event) {
   let x = event.offsetX;
   let y = event.offsetY;
   coords.value = `Coords: ${x} : ${y}`;
+  if (Math.abs(event.offsetX - treasure.x) < 60) {
+    // умова, де знаходиться скарб - відстань від координат курсору до координат скарбу менше 30
+    //alert("Скарб тут!");
+    let treasureCircle = document.createElement("div");
+    treasureCircle.style =
+      "border: solid red; width: 120px; height: 120px; position: absolute; top:0; left:0;";
+    wrap.appendChild(treasureCircle);
+    alert("Скарб тут!");
+  }
 };
